296304d4098ab52df43723ad334aaef5f5d35013
Shane Dempsey <shane@vearsa.com>
2016-08-04 16:22:45 +0100

    jhgjhg6

--- build.sh
+++ build.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 set -e -x
+
+BUILD_DIR=$PWD/built-resource
+mkdir -p $BUILD_DIR
 BASE_DIR=$(readlink -f $(dirname $0))
 
 echo "$(hostname) $(date)" > $BASE_DIR/output-1.0.0.txt
 echo "$(hostname) $(date)" > /output-1.0.0.txt
 echo "$(hostname) $(date)" > output-1.0.0.txt
+echo "$(hostname) $(date)" > $BUILD_DIR/output-1.0.0.txt
 
 ls -ltr /
 ls -ltr $BASE_DIR
