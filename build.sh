b30e4df81160e2e44959b56f4e63b9189fa42697
Shane Dempsey <shane@vearsa.com>
2016-08-04 16:12:00 +0100

    jhgjhg6

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ BASE_DIR=$(readlink -f $(dirname $0))
 
 echo "$(hostname) $(date)" > $BASE_DIR/output-1.0.0.txt
 echo "$(hostname) $(date)" > /output-1.0.0.txt
+echo "$(hostname) $(date)" > output-1.0.0.txt
 
 ls -ltr /
 ls -ltr $BASE_DIR
