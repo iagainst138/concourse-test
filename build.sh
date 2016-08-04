fca10701c5efa638a59fa2e7b4879496213770f6
Shane Dempsey <shane@vearsa.com>
2016-08-04 15:39:46 +0100

    jhgjhg6

--- build.sh
+++ build.sh
@@ -5,10 +5,9 @@ set -e -x
 #echo "$(hostname) $(date)" > output-$(date +%s).txt
 echo "$(hostname) $(date)" > /output-1.0.0.txt
 
-ls -l output-*.txt
 ls -l /
 pwd
-dirname $0
+readlink -f $(dirname $0)
 echo
 
 ls -ltr
