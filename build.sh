aea53b4aa5d15b0dc963b6698eca6cb52ee028c8
Shane Dempsey <shane@vearsa.com>
2016-08-04 15:38:53 +0100

    jhgjhg6

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 set -e -x
 
 #echo "$(hostname) $(date)" > output-$(date +%s).txt
-echo "$(hostname) $(date)" > output-1.0.0.txt
+echo "$(hostname) $(date)" > /output-1.0.0.txt
 
 ls -l output-*.txt
 ls -l /
