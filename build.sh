5e62cc72ea0e93e50d8f8df7f487e82f3d9dd9e9
Shane Dempsey <shane@vearsa.com>
2016-08-05 07:59:09 +0100

    jhgjhg6

--- build.sh
+++ build.sh
@@ -9,10 +9,10 @@ BASE_DIR=$(readlink -f $(dirname $0))
 echo > /dev/null
 
 echo "$(hostname) $(date)" > $BASE_DIR/output-1.0.0.txt
-echo "$(hostname) $(date)" > /output-1.0.0.txt
+#echo "$(hostname) $(date)" > /output-1.0.0.txt
 echo "$(hostname) $(date)" > output-1.0.0.txt
-echo "$(hostname) $(date)" > $BUILD_DIR/output-1.0.0.txt
+#echo "$(hostname) $(date)" > $BUILD_DIR/output-1.0.0.txt
 
-ls -ltr /
-ls -ltr $BASE_DIR
-ls -ltr
+#ls -ltr /
+#ls -ltr $BASE_DIR
+ls -ltrR
