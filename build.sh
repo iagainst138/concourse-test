403e4850d30c57e34913d268dd5e1a82c6c08ec4
Shane Dempsey <shane@vearsa.com>
2016-08-05 08:30:52 +0100

    jhgjhg6

--- build.sh
+++ build.sh
@@ -11,7 +11,7 @@ echo > /dev/null
 echo "$(hostname) $(date)" > $BASE_DIR/output-1.0.0.txt
 #echo "$(hostname) $(date)" > /output-1.0.0.txt
 echo "$(hostname) $(date)" > output-1.0.0.txt
-#echo "$(hostname) $(date)" > $BUILD_DIR/output-1.0.0.txt
+echo "$(hostname) $(date)" > $BUILD_DIR/output-1.0.0.txt
 
 #ls -ltr /
 #ls -ltr $BASE_DIR
