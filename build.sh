5c6834e27c3d1df608f594a906d4fa738e84ebf8
Shane Dempsey <shane@vearsa.com>
2016-08-05 15:31:32 +0100

    2016-08-05_15:31:32

--- build.sh
+++ build.sh
@@ -4,18 +4,7 @@ set -e -x
 
 BUILD_DIR=$PWD/built-resources
 mkdir -p $BUILD_DIR
-BASE_DIR=$(readlink -f $(dirname $0))
 
-#echo "$(hostname) $(date)" > $BASE_DIR/output-1.0.0.txt
-#echo "$(hostname) $(date)" > /output-1.0.0.txt
-#echo "$(hostname) $(date)" > output-1.0.0.txt
 echo "$(hostname) $(date)" > $BUILD_DIR/output-1.0.$(date +%s).txt
 
-#ls -ltr /
-#ls -ltr $BASE_DIR
 ls -ltrR
-
-
-
-
-
